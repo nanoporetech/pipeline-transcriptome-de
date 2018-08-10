f64d66e850a255079ed93690c84c41ec5cb92f9f
Botond Sipos <Botond.Sipos@nanoporetech.com>
2018-08-10 14:30:21 +0100

    Working DE analysis.

--- scripts/deseq2_analysis.R
+++ scripts/deseq2_analysis.R
@@ -1,3 +1,17 @@
 #!/usr/bin/env Rscript
 
+cat("Loading DESeq2.\n")
 suppressMessages(library("DESeq2"))
+
+cat("Loading counts and conditions.\n")
+cts <- as.matrix(read.csv("merged/all_counts.tsv", sep="\t", row.names="Reference"))
+coldata <- read.csv("de_analysis/coldata.tsv", row.names="sample", sep="\t")
+
+cat("Performing differential expression analysis.")
+dds <- DESeqDataSetFromMatrix(countData = cts, colData = coldata, design = ~ condition)
+dds <- DESeq(dds)
+res <- results(dds, alpha=0.05)
+resOrdered <- res[order(res$pvalue),]
+
+head(resOrdered)
+write.csv(as.data.frame(resOrdered), file="de_analysis/deseq2_results.tsv", sep="\t")
